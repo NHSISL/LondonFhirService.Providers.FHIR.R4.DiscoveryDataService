a0e79b8cfcdbf477a0a46e687e62ff955534fa29
Mehran Rashid <mehran.rashid@answerdigital.com>
2025-11-17 14:54:24 +0000

    INFRA: Upgrade to .NET 10

--- LondonFhirService.Providers.FHIR.R4.DiscoveryDataService.Infrastructure/Program.cs
+++ LondonFhirService.Providers.FHIR.R4.DiscoveryDataService.Infrastructure/Program.cs
@@ -15,7 +15,7 @@ namespace LondonFhirService.Providers.FHIR.R4.DiscoveryDataService.Infrastructur
             scriptGenerationService.GenerateBuildScript(
                 branchName: "main",
                 projectName: "LondonFhirService.Providers.FHIR.R4.DiscoveryDataService",
-                dotNetVersion: "9.0.100");
+                dotNetVersion: "10.0.100");
 
             scriptGenerationService.GeneratePrLintScript(branchName: "main");
         }
